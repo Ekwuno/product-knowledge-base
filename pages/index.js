15e009e24fff8f060f840ad0ad072f9f8f0bea7c
Ekwuno <obinnacodes@gmail.com>
2021-09-22 13:07:53 +0100

    fix error so build will go through

--- pages/index.js
+++ pages/index.js
@@ -50,28 +50,28 @@ export const getStaticProps = async () => {
 
 export default function Home({ product }) {
   return (
-    <div class="font-sans text-gray-900 h-full">
-      <header class="absolute top-0 left-0 w-full py-4">
-        <div class="flex items-center justify-between px-8">
-          <span class="text-2xl tracking-tighter font-semibold">
+    <div className="font-sans text-gray-900 h-full">
+      <header className="absolute top-0 left-0 w-full py-4">
+        <div className="flex items-center justify-between px-8">
+          <span className="text-2xl tracking-tighter font-semibold">
             Egghead-Demo
           </span>
-          <div class="">
-            <div class="flex items-center">
+          <div className="">
+            <div className="flex items-center">
               <a
-                class="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
+                className="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
                 href="#"
               >
                 Home
               </a>
               <a
-                class="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
+                className="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
                 href="#"
               >
                 Products
               </a>
               <a
-                class="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
+                className="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
                 href="#"
               >
                 Docs
@@ -82,23 +82,23 @@ export default function Home({ product }) {
       </header>
       {/* <Blog/> */}
 
-      <section class="bg-blue-100 h-full py-8">
-        <div class="w-5/6 max-w-lg ml-auto mr-auto h-full">
-          <div class="flex items-center justify-center text-center h-full">
+      <section className="bg-blue-100 h-full py-8">
+        <div className="w-5/6 max-w-lg ml-auto mr-auto h-full">
+          <div className="flex items-center justify-center text-center h-full">
             <div>
-              <h1 class="text-4xl sm:text-5xl font-semibold leading-none tracking-tighter mb-4">
+              <h1 className="text-4xl sm:text-5xl font-semibold leading-none tracking-tighter mb-4">
                 {product.landingPageCollection.items[0].header}
               </h1>
-              <h2 class="text-2xl sm:text-3xl text-blue-800 opacity-75 font-normal leading-tight mb-8">
+              <h2 className="text-2xl sm:text-3xl text-blue-800 opacity-75 font-normal leading-tight mb-8">
                 {product.landingPageCollection.items[0].subHeader}
               </h2>
 
               <Link href="/blog">
-                <a class="flex flex-col sm:flex-row justify-center pt-8">
-                  <button class="bg-blue-500 hover:bg-blue-600 text-2xl leading-none text-white font-semibold h-12 px-8 rounded-full whitespace-no-wrap mb-2 sm:mb-0 sm:mr-2">
+                <a className="flex flex-col sm:flex-row justify-center pt-8">
+                  <button className="bg-blue-500 hover:bg-blue-600 text-2xl leading-none text-white font-semibold h-12 px-8 rounded-full whitespace-no-wrap mb-2 sm:mb-0 sm:mr-2">
                     Get started
                   </button>
-                  <button class="bg-transparent text-2xl leading-none text-blue-500 font-semibold hover:text-blue-600 h-12 px-8 border border-blue-200 hover:border-blue-400 rounded-full whitespace-no-wrap mt-2 sm:mt-0 sm:ml-2">
+                  <button className="bg-transparent text-2xl leading-none text-blue-500 font-semibold hover:text-blue-600 h-12 px-8 border border-blue-200 hover:border-blue-400 rounded-full whitespace-no-wrap mt-2 sm:mt-0 sm:ml-2">
                     Learn more
                   </button>
                 </a>
@@ -109,7 +109,7 @@ export default function Home({ product }) {
       </section>
 
       {product.postCollection.items.map((place) => (
-        <div>
+        <div key= {place.slug}>
           <div className={styles.grid}>
             <div className={styles.card}>
               <div></div>
