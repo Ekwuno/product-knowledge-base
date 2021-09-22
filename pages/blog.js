46bc7d8bb34518cefd09e9f6fdeaee9779e11fbf
Ekwuno <obinnacodes@gmail.com>
2021-09-22 12:28:23 +0100

    added forms and landing page

--- pages/blog.js
+++ pages/blog.js
@@ -1,137 +1,62 @@
-import { GraphQLClient, gql } from "graphql-request";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+import styles from "../styles/Home.module.css"
 
-export const getStaticProps = async () => {
-  const endpoint = `https://graphql.contentful.com/content/v1/spaces/${process.env.Space_ID}`;
-
-  const graphQLClient = new GraphQLClient(endpoint, {
-    headers: {
-      authorization: `Bearer ${process.env.CDA_Token}`,
-    },
-  });
-
-  const Productquery = gql`
-    {
-      landingPageCollection {
-        items {
-          header
-          subHeader
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
         }
       }
-    }
-  `;
+    `,
+  });
 
-  const product = await graphQLClient.request(Productquery);
-  console.log(product);
   return {
-    props: { product },
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
   };
-};
+}
 
-export default function Blog({ product }) {
+export default function Blog({ countries }) {
   return (
-    <div class="font-sans text-gray-900 h-full">
-        
-      <header class="absolute top-0 left-0 w-full py-4">
-        <div class="flex items-center justify-between px-8">
-          <span class="text-2xl tracking-tighter font-semibold">AppLogo</span>
-          <div class="">
-            <div class="flex items-center">
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        {countries.map((country) => (
+          <div key={country.code} className={styles.card}>
+            <h3>
               <a
-                class="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
-                href="#"
+                href="#country-name"
+                aria-hidden="true"
+                class="aal_anchor"
+                id="country-name"
               >
-                Link
-              </a>
-              <a
-                class="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
-                href="#"
-              >
-                Link
-              </a>
-              <a
-                class="text-lg leading-normal text-blue-500 hover:text-blue-600 no-underline ml-8"
-                href="#"
-              >
-                Link
-              </a>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      <section class="bg-blue-100 h-full py-8">
-        <div class="w-5/6 max-w-lg ml-auto mr-auto h-full">
-          <div class="flex items-center justify-center text-center h-full">
-            <div>
-              <h1 class="text-4xl sm:text-5xl font-semibold leading-none tracking-tighter mb-4">
-                {product.landingPageCollection.items[0].header}
-              </h1>
-              <h2 class="text-2xl sm:text-3xl text-blue-800 opacity-75 font-normal leading-tight mb-8">
-                {product.landingPageCollection.items[0].subHeader}
-              </h2>
-              <div class="flex flex-col sm:flex-row items-center justify-center sm:pt-3 mb-3 sm:mb-6">
-                <a
-                  class="flex items-center text-xl leading-none text-blue-500 hover:text-blue-600 no-underline mb-2 sm:mb-0 sm:mr-4"
-                  href="#"
+                <svg
+                  aria-hidden="true"
+                  class="aal_svg"
+                  height="16"
+                  version="1.1"
+                  viewBox="0 0 16 16"
+                  width="16"
                 >
-                  <div class="mr-3">
-                    <svg
-                      class="align-middle"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <circle cx="12" cy="5" r="3" />
-                      <line x1="12" y1="22" x2="12" y2="8" />
-                      <path d="M5 12H2a10 10 0 0 0 20 0h-3" />
-                    </svg>
-                  </div>
-                  <span>Link to something</span>
-                </a>
-                <a
-                  class="flex items-center text-xl leading-none text-blue-500 hover:text-blue-600 no-underline mt-2 sm:mt-0 sm:ml-4"
-                  href="#"
-                >
-                  <div class="mr-3">
-                    <svg
-                      class="align-middle"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <circle cx="12" cy="12" r="10" />
-                      <polyline points="8 12 12 16 16 12" />
-                      <line x1="12" y1="8" x2="12" y2="16" />
-                    </svg>
-                  </div>
-                  <span>Link to something</span>
-                </a>
-              </div>
-              <div class="flex flex-col sm:flex-row justify-center pt-8">
-                <button class="bg-blue-500 hover:bg-blue-600 text-2xl leading-none text-white font-semibold h-12 px-8 rounded-full whitespace-no-wrap mb-2 sm:mb-0 sm:mr-2">
-                  Get started
-                </button>
-                <button class="bg-transparent text-2xl leading-none text-blue-500 font-semibold hover:text-blue-600 h-12 px-8 border border-blue-200 hover:border-blue-400 rounded-full whitespace-no-wrap mt-2 sm:mt-0 sm:ml-2">
-                  Learn more
-                </button>
-              </div>
-            </div>
+                  <path
+                    fill-rule="evenodd"
+                    d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+                  ></path>
+                </svg>
+              </a>
+              {country.name}
+            </h3>
+            <p>
+              {country.code} - {country.emoji}
+            </p>
           </div>
-        </div>
-      </section>
-      {product.landingPageCollection.items[0].header}
+        ))}
+      </div>
     </div>
   );
 }
